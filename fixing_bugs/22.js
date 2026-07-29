4b17bfc4f4fc1fcb4da7755905086a7313cef0e7
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-07-29 23:55:48 +0600

    prac

---
+++ fixing_bugs/22.js
@@ -0,0 +1,11 @@
+function average(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum / (arr.length);
+}
+
+console.log(average([10, 20, 30, 40])); // Expected: 25
