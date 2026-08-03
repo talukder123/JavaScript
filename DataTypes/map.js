d323484caf5ddd18384067fed04e8af22afc30ba
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-08-03 20:20:02 +0600

    new commit

--- DataTypes/map.js
+++ DataTypes/map.js
@@ -1,6 +1,11 @@
 let arr = [2, 4, 6, 8 ,10]
 
-const double = arr.map(function(i) { // using callback function!
+// template --:
+// const valiable_name = array_name.map_keyword( pass the function -> (iterator) => {
+//        your conditions ........
+// })
+
+const double = arr.map((i) => { // using callback function! | arrow function
     return i*2
 } )
 
