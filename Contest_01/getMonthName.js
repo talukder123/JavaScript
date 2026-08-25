247e7e8ae800422246c57cdb81af5b039497c54a
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-08-25 23:29:33 +0600

    contest

---
+++ Contest_01/getMonthName.js
@@ -0,0 +1,27 @@
+function getMonthName(monthNumber) {
+  if (monthNumber === 1) {
+    return "January";
+  } else if (monthNumber === 2) {
+    return "February";
+  } else if (monthNumber === 3) {
+    return "March";
+  } else if (monthNumber === 4) {
+    return "April";
+  } else if (monthNumber === 5) {
+    return "May";
+  } else if (monthNumber === 6) {
+    return "June";
+  } else if (monthNumber === 7) {
+    return "July";
+  } else if (monthNumber === 8) {
+    return "August";
+  } else if (monthNumber === 9) {
+    return "September";
+  } else if (monthNumber === 10) {
+    return "October";
+  } else if (monthNumber === 11) {
+    return "November";
+  } else if (monthNumber === 12) {
+    return "December";
+  }
+}
