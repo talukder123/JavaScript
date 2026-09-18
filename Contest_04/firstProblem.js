699aa51332987e9be3e7367c6840e22d6dbd5c96
Abdus Salam Talukder <salamtalukder.official@gmail.com>
2026-09-18 22:32:44 +0600

    C4-P1 solved

---
+++ Contest_04/firstProblem.js
@@ -0,0 +1,16 @@
+function isAnagram(s1, s2) {
+    let str1 = s1.toLowerCase()
+    let str2 = s2.toLowerCase()
+
+    let first = str1.replace(/[^a-z0-9]/g, "");
+    let second = str2.replace(/[^a-z0-9]/g, "");
+
+    first = first.split("").sort().join("");
+    second = second.split("").sort().join("");
+
+    if (first === second) {
+        return true;
+    }
+
+    return false;
+}
